c407d208a52bf21f4534a6d745973c8e6736dd2b
Leen Husseini <leen.husseini05@gmail.com>
2024-12-25 14:44:34 +0300

    Checkout page controller and view

--- AirlineManagementSystem/Controllers/FlightsController.cs
+++ AirlineManagementSystem/Controllers/FlightsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using AirlineManagementSystem.Models;
 using Npgsql;
 using AirlineManagementSystem.ViewModels;
+using System.Runtime.CompilerServices;
 
 namespace AirlineManagementSystem.Controllers{
     public class FlightsController : Controller{
@@ -32,7 +33,8 @@ namespace AirlineManagementSystem.Controllers{
                 ""Flight"".""ArrivalTime"",
                 ""Flight"".""Price"",
                 ""Airline"".""AirlineName"",
-                ""Flight"".""FlightID""
+                ""Flight"".""FlightID"",
+                flightduration(""Flight"".""FlightID"") AS Duration
             FROM ""Flight""
             INNER JOIN ""Plane"" ON ""Flight"".""PlaneID"" = ""Plane"".""PlaneID""
             INNER JOIN ""Airline"" ON ""Airline"".""AirlineID"" = ""Plane"".""AirlineID""
@@ -65,30 +67,100 @@ namespace AirlineManagementSystem.Controllers{
                             ArrivalTime = reader.GetDateTime(reader.GetOrdinal("ArrivalTime")),
                             Price = reader.GetDecimal(reader.GetOrdinal("Price")),
                             AirlineName = reader.GetString(reader.GetOrdinal("AirlineName")),
-                            FlightID = reader.GetInt32(reader.GetOrdinal("FlightID"))
+                            FlightID = reader.GetInt32(reader.GetOrdinal("FlightID")),
+                            Duration = reader.GetString(reader.GetOrdinal("Duration"))
                         });
                     }
                 }
             }
         }
-
-        
-
             return View(flights);
          }
 
+
          [HttpGet]
         public IActionResult Checkout(int FlightID) {
-
             return View();
         }
+
+       
+
+      
+        [HttpGet]
+        public IActionResult GetBasePrice(int FlightID)
+        {
+            decimal basePrice;
+            string query = @$"SELECT ""Price"" FROM ""Flight"" WHERE ""FlightID"" = @FlightID";
+
+            using(var connection = new NpgsqlConnection(_connectionString))
+            {
+                connection.Open();
+                using(var command = new NpgsqlCommand(query,connection))
+                {
+                    command.Parameters.AddWithValue("@FlightID", FlightID);
+                    basePrice = (decimal)command.ExecuteScalar();
+                }
+            }
+
+            return Json(new {basePrice});
+        }
+
+          private decimal CallTotalPrice(int FlightID, int TicketTypeID, int BaggageTypeID)
+        {
+            string query = @$"SELECT ""GetTotalPrice""(@flightid, @tickettypeid, @baggagetypeid)";
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {   
+                connection.Open();
+                using(var command = new NpgsqlCommand(query,connection))
+                {
+                    command.Parameters.AddWithValue("@flightid",FlightID);
+                    command.Parameters.AddWithValue("@tickettypeid", TicketTypeID);
+                    command.Parameters.AddWithValue("@baggagetypeid", BaggageTypeID);
+                    return Convert.ToDecimal(command.ExecuteScalar());
+                }
+
+              
+            }
+        }
+
+     
+
         [HttpPost]
-        public IActionResult Checkout(Ticket model) {
+        public IActionResult GetTotalPrice(int FlightID, int TicketTypeID, int BaggageTypeID)
+        {
+            decimal totalPrice = CallTotalPrice(FlightID, TicketTypeID,BaggageTypeID);
+            return Json(new{totalPrice});
+        }
 
+     
+        [HttpPost]
+        public IActionResult LastCheckoutPost(decimal totalPrice){
+          return RedirectToAction("LastCheckout", new {totalPrice});
+        }
+
+      
+        [HttpGet]
+        public IActionResult LastCheckout(decimal totalPrice)
+        {
+            ViewData["TotalPrice"] = totalPrice;
             return View();
         }
-        public IActionResult LastCheckout(){
-            return View();
+
+        public void savebaggageid(int ticketid, int baggagetypeid)
+        {
+            string query = @" UPDATE ""Baggage""(""TicketID"",""BaggageTypeID"") VALUES (@TicketID, @BaggageTypeID)";
+
+            using(var connection = new NpgsqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = new NpgsqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@TicketID", ticketid);
+                    command.Parameters.AddWithValue("BaggageTypeID", baggagetypeid);
+                    command.ExecuteNonQuery();
+                }
+            }
         }
+
     }
 }
